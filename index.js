4b31e62c3abe36ecd12237c41738125e4f47b770
MichelleAaa <99776009+MichelleAaa@users.noreply.github.com>
2022-08-04 15:30:10 -0400

    Added Class Notes

--- index.js
+++ index.js
@@ -7,35 +7,37 @@ const connect = mongoose.connect(url, {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+}); //These are being set to deal with deprecation warnings from mongo.
 
-connect.then(() => {
+connect.then(() => { //connect method contains a promise, so we can chain a .then() method to it. 
 
     console.log('Connected correctly to server');
 
     Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    })
+    }) //Takes an object that defines the new object to create.
+//The create method resolves a promise, so we can use it as the start of our promise chain.
+
     .then(campsite => {
         console.log(campsite);
 
         return Campsite.findByIdAndUpdate(campsite._id, {
-            $set: { description: 'Updated Test Document' }
-        }, {
+            $set: { description: 'Updated Test Document' } //This is the field we want to change, in this case, the description. 
+        }, { //The third argument will cause this method to return the updated document. (The default is that it will return the original document, prior to the update.)
             new: true
         });
     })
     .then(campsite => {
         console.log(campsite);
 
-        campsite.comments.push({
+        campsite.comments.push({ //The comments subdocument is stored in an array, so we are able to use the push method to push in a new object document.
             rating: 5,
             text: 'What a magnificent view!',
             author: 'Tinus Lorvaldes'
         });
 
-        return campsite.save();
+        return campsite.save(); //Saves the document to the database, and then returns a promise that indicates if it worked or not.
     })
     .then(campsite => {
         console.log(campsite);
--- models/campsite.js
+++ models/campsite.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
     rating: {
-        type: Number,
+        type: Number, //when you use a type of number, you can specify the boundaries for it.
         min: 1,
         max: 5,
         required: true
@@ -24,17 +24,19 @@ const campsiteSchema = new Schema({
     name: {
         type: String,
         required: true,
-        unique: true
+        unique: true //No two documents should have the same value in the name field.
     },
     description: {
         type: String,
         required: true
     },
-    comments: [commentSchema]
+    comments: [commentSchema] //We add in the commentSchema inside the campsiteScheme. 
 }, {
-    timestamps: true
+    timestamps: true //This is a second optional argument. In this case, we are using timestamps as true. This will cause mongoose to add two values, ‘created at’ and added at. When a document is created it will be given a ‘created at’ and ‘updated at’ property. Thereafter, whenever it’s updated, the ‘updated at’ property will be updated by mongoose. 
 });
 
-const Campsite = mongoose.model('Campsite', campsiteSchema);
+const Campsite = mongoose.model('Campsite', campsiteSchema); //This creates a model named Campsite, and the first argument is the capitalized and singular version of the collection you want to use for the model. Mongoose will automatically look for the lowercase *** version to look for the collection. So Campsite is the first argument, while the second argument is the schema we want to use for the collection. In this case, campsiteSchema.
+//this returns a constructor function. 
+//mongoose was written in 2010 so it doesn’t use classes. The model is a desugared class in a way. It’s being used to instantiate objects, or in this case, instantiate documents for MongoDB.
 
 module.exports = Campsite;
